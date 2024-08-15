c0b345d2d15c8e469b5175be4d6620108cffe293
UrbsKali <urbain.lantres@tutanota.com>
2024-08-15 19:31:25 +0200

    Update UpdateOverlay.go

--- utils/UpdateOverlay.go
+++ utils/UpdateOverlay.go
@@ -27,12 +27,20 @@ func UpdateOverlay() {
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	err = archiver.Unarchive("admin.zip", "admin")
+	err = DownloadFile("endgame.zip", "https://github.com/Urbskali/oblivionDraft/releases/latest/download/endgame.zip")
 	if err != nil {
 		log.Fatal(err)
 	}
-	err = archiver.Unarchive("overlay.zip", "overlay")
+
+	err = archiver.Unarchive("admin.zip", "tmp_admin")
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = archiver.Unarchive("overlay.zip", "tmp_overlay")
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = archiver.Unarchive("endgame.zip", "tmp_endgame")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -44,21 +52,20 @@ func UpdateOverlay() {
 	if err != nil {
 		log.Fatal(err)
 	}
+	err = os.Remove("endgame.zip")
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	// Move the files to the correct location
-	err = os.Rename("admin", "tmp_admin")
-	if err != nil {
-		log.Fatal(err)
-	}
-	err = os.Rename("overlay", "tmp_overlay")
-	if err != nil {
-		log.Fatal(err)
-	}
 	err = os.Rename("tmp_admin/admin", "admin")
 	if err != nil {
 		log.Fatal(err)
 	}
 	err = os.Rename("tmp_overlay/overlay", "overlay")
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = os.Rename("tmp_endgame/endgame", "endgame")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -70,8 +77,12 @@ func UpdateOverlay() {
 	if err != nil {
 		log.Fatal(err)
 	}
+	err = os.RemoveAll("tmp_endgame")
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	log.Print("Overlay and admin updated")
+	log.Print("Overlays are updated")
 }
 
 func DownloadFile(filepath string, url string) error {
