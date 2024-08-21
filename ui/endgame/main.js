356a0270435d9118f5785597defb466281101b12
UrbsKali <urbain.lantres@tutanota.com>
2024-08-21 18:30:25 +0200

    ENDGAME : Change teams color

--- ui/endgame/main.js
+++ ui/endgame/main.js
@@ -196,7 +196,7 @@ new Chart(ctx, {
             cubicInterpolationMode: 'monotone',
             pointStyle: false,
             borderColor: 'black',
-            fill: { "above": "#6586ffa0", "below": "#ff8553a0", "target": { "value": 0 } },
+            fill: { "above": "#6586ffa0", "below": "#eb0808a0", "target": { "value": 0 } },
             borderWidth: 1,
         }]
 
